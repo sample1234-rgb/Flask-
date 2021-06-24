feb8161322f2a59b0ca0d9cb8b61e8674fc3c82a
Gaurav Bhardwaj <bhardwajg2411@gmail.com>
2021-06-24 11:08:44 +0530

    Initiate

---
+++ hello.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('hello.html')
+
+@app.route('/resume')
+def Resume():
+    edu = {'name': 'Education', 'points': [
+        {'quali': 'B. Tech.', 'duration': '7/2018 - -', 'name': 'NIT J', 'score': '6.96'},
+        {'quali': 'Higher', 'duration': '4/2016 - 4/2018', 'name': 'APJ School', 'score': '9.35'},
+        {'quali': 'Matrix', 'duration': '4/2015 - 4/2016', 'name': 'APJ School', 'score': '8.8'},
+    ]}
+    skil = {'name': 'Skills', 'points': ['C/C++', 'Python', 'Django', 'Data Analytics', 'OOPs', 'Data Structures',
+                                         'DataBase Management System', 'Operating System', 'ReactJs', '3* CC']}
+    data = [edu, skil]
+    return render_template('resume.html', data=data)
+
+@app.route('/projects')
+def project():
+    projects = [
+        {'no': 1, 'name': 'Resume builder GUI', 'desc': 'Resume builder for desktops build with Python + Tkinter'},
+        {'no': 2, 'name': 'Search Visualizer', 'desc': 'Using various Algorithms used for searching and finding elements'
+         },
+        {'no': 3, 'name': 'Record keeper', 'desc': 'A simple record keeping application for schools/colleges/organizations'
+         }
+    ]
+    return render_template('Project.html', projects=projects)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
