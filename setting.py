a77953476fdba28d1c2a01e6396e90b950dd8704
Gaurav Bhardwaj <bhardwajg2411@gmail.com>
2021-07-16 17:10:31 +0530

    Major Changes: Layout + exp(/glass-morphism)

---
+++ setting.py
@@ -0,0 +1,12 @@
+
+def initiate(app):
+    app.config['SECRET_KEY'] = 'Selena | Gaurav'
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USERNAME'] = 'bhardwajg2411@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'dakapqctjyffjrrc'
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+    # return app
